e3a11becf357260d0b9f47c1751a064167f282d5
sivak <sivak@192.168.0.12>
2018-05-17 20:05:31 -0400

    Commit Jenkins

---
+++ RunMe.bat
@@ -0,0 +1,9 @@
+set projectLocation=C:\Users\sivak\Desktop\vani\ToolsDemo
+
+cd %projectLocation%
+
+set classpath=%projectLocation%\bin;%projectLocation%\Jars\*
+
+java org.testng.TestNG %projectLocation%\testng.xml
+
+pause
