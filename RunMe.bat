c358d464170c71806f832f83390ba493bf25c11f
sivak <sivak@192.168.0.12>
2018-05-18 22:09:24 -0400

    Commit for Jenkins

--- RunMe.bat
+++ RunMe.bat
@@ -2,6 +2,7 @@ set projectLocation=C:\Users\sivak\Desktop\vani\ToolsDemo
 
 cd %projectLocation%
 
+
 set classpath=%projectLocation%\bin;%projectLocation%\Jars\*
 
 java org.testng.TestNG %projectLocation%\testng.xml
--- src/GIT/gitDemo.java
+++ src/GIT/gitDemo.java
@@ -1,13 +1,23 @@
 package GIT;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class gitDemo 
 {
+	
 @Test
 public void gitoutput()
 {
+	System.setProperty("webdriver.chrome.driver","C:\\Users\\sivak\\Desktop\\vani\\ToolsDemo\\Drivers\\chromedriver.exe");
+	WebDriver driver =new ChromeDriver();
 	System.out.println("This is the GIT Demo");
+	driver.get("https://google.com");
+	driver.findElement(By.name("q")).sendKeys("Edureka",Keys.ENTER);
+
 }
 	
 }
